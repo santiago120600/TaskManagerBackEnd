324fd44f71e43e90af9b1fa8faab3b24c659aca7
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-08-20 15:08:13 -0500

    changed GET methods

--- user/models.py
+++ user/models.py
@@ -8,6 +8,9 @@ class User(models.Model):
     name_user = models.CharField(max_length=80)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.user_email
+
 class Folder(models.Model):
     id_folder  = models.AutoField(primary_key=True)
     name_folder = models.CharField(max_length=80)
@@ -15,22 +18,31 @@ class Folder(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now= True)
 
+    def __str__(self):
+        return self.name_folder
+
 class Task(models.Model):
     id_task  = models.AutoField(primary_key=True)
     img_task = models.CharField(max_length=150)
     desc_task = models.TextField()
     completed = models.BooleanField(default=False)
-    user  = models.ForeignKey(User, null=False,on_delete=models.CASCADE)
-    folder = models.ForeignKey(Folder, null=False,on_delete=models.CASCADE)
+    user  = models.ForeignKey(User, null=False,on_delete=models.CASCADE, related_name="users")
+    folder = models.ForeignKey(Folder, related_name="tasks", null=False,on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now= True)
+
+    def __str__(self):
+        return self.desc_task
 
 class Sub_task(models.Model):
     id_sub_task = models.AutoField(primary_key=True)
     desc_sub_task = models.TextField()
     completed = models.BooleanField(default=False)
-    task = models.ForeignKey(Task, null=False,on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, null=False,on_delete=models.CASCADE, related_name="subtasks")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now= True)
+
+    def __str__(self):
+        return self.desc_sub_task
 
 
--- user/serializers.py
+++ user/serializers.py
@@ -1,22 +1,28 @@
 from rest_framework import serializers
 from user.models import User, Folder, Task, Sub_task
 
+class SubTaskSerializer(serializers.ModelSerializer):
+    task_name = serializers.ReadOnlyField(source='task.desc_task')
+    class Meta:
+        model = Sub_task
+        fields = "__all__"
+
+class TaskSerializer(serializers.ModelSerializer):
+    user_name = serializers.CharField(source='user.name_user')
+    folder_name = serializers.CharField(source='folder.name_folder')
+    subtasks = SubTaskSerializer('subtasks',many=True)
+    class Meta:
+        model = Task
+        fields =  ('id_task','img_task','desc_task','completed','user','user_name','folder','folder_name','created_at','updated_at', 'subtasks')
+
 class UserSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer(source='users',many=True)
     class Meta:
         model = User
         fields = '__all__'
 
 class FolderSerializer(serializers.ModelSerializer):
+    tasks = TaskSerializer('tasks',many=True)
     class Meta:
         model = Folder
-        fields = '__all__'
-
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Task
-        fields = '__all__'
-
-class SubTaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Sub_task
         fields = '__all__'
