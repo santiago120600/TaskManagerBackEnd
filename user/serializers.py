c89f427f6754945c537a8768554264e08dfcc57f
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-08-23 13:07:58 -0500

    removed the required fields in POST

--- user/serializers.py
+++ user/serializers.py
@@ -8,21 +8,21 @@ class SubTaskSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class TaskSerializer(serializers.ModelSerializer):
-    user_name = serializers.CharField(source='user.name_user')
-    folder_name = serializers.CharField(source='folder.name_folder')
-    subtasks = SubTaskSerializer('subtasks',many=True)
+    user_name = serializers.ReadOnlyField(source='user.name_user')
+    folder_name = serializers.ReadOnlyField(source='folder.name_folder')
+    subtasks = SubTaskSerializer('subtasks',many=True, read_only=True)
     class Meta:
         model = Task
         fields =  ('id_task','img_task','desc_task','completed','user','user_name','folder','folder_name','created_at','updated_at', 'subtasks')
 
 class UserSerializer(serializers.ModelSerializer):
-    tasks = TaskSerializer(source='users',many=True)
+    tasks = TaskSerializer(source='users',many=True, read_only=True)
     class Meta:
         model = User
         fields = '__all__'
 
 class FolderSerializer(serializers.ModelSerializer):
-    tasks = TaskSerializer('tasks',many=True)
+    tasks = TaskSerializer('tasks',many=True, read_only=True)
     class Meta:
         model = Folder
         fields = '__all__'
