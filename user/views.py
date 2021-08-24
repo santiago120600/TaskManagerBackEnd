6f3250eaa7f8872ed513b22b764c1fb13b63d3a6
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-08-24 12:42:57 -0500

    GET items by id

--- user/views.py
+++ user/views.py
@@ -11,8 +11,12 @@ from user.serializers import UserSerializer, FolderSerializer, TaskSerializer, S
 @api_view(['GET','POST','DELETE','PUT'])
 def userApi(request,id=0):
     if request.method == 'GET':
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
+        if id is not 0:
+            user = User.objects.get(id_user=id)
+            serializer = UserSerializer(user, many=False)
+        else:
+            users = User.objects.all()
+            serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = UserSerializer(data=request.data)
@@ -34,8 +38,12 @@ def userApi(request,id=0):
 @api_view(['GET','POST','DELETE','PUT'])
 def folderApi(request,id=0):
     if request.method == 'GET':
-        folders = Folder.objects.all()
-        serializer = FolderSerializer(folders, many=True)
+        if id is not 0:
+            folder = Folder.objects.get(id_folder=id)
+            serializer = FolderSerializer(folder, many=False)
+        else:
+            folders = Folder.objects.all()
+            serializer = FolderSerializer(folders, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = FolderSerializer(data=request.data)
@@ -57,8 +65,12 @@ def folderApi(request,id=0):
 @api_view(['GET','POST','DELETE','PUT'])
 def taskApi(request,id=0):
     if request.method == 'GET':
-        task = Task.objects.all()
-        serializer = TaskSerializer(task, many=True)
+        if id is not 0:
+            task = Task.objects.get(id_task=id)
+            serializer = TaskSerializer(task, many=False)
+        else:
+            tasks = Task.objects.all()
+            serializer = TaskSerializer(tasks, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = TaskSerializer(data=request.data)
@@ -80,8 +92,12 @@ def taskApi(request,id=0):
 @api_view(['GET','POST','DELETE','PUT'])
 def subtaskApi(request,id=0):
     if request.method == 'GET':
-        subtask = Sub_task.objects.all()
-        serializer = SubTaskSerializer(subtask, many=True)
+        if id is not 0:
+            subtask = Sub_task.objects.get(id_sub_task=id)
+            serializer = SubTaskSerializer(subtask, many=False)
+        else:
+            subtasks = Sub_task.objects.all()
+            serializer = SubTaskSerializer(subtasks, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
         serializer = SubTaskSerializer(data=request.data)
